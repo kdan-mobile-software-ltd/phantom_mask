d7c9410503acd874423f89b223245844ce1a0244
kyle <kyle2542736@gmail.com>
2025-04-22 01:21:13 +0800

    feat: 新增 Swagger OpenApiConfig

---
+++ demo/src/main/java/KADAN/interview/demo/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package KADAN.interview.demo.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Mask Service API")
+						.version("1.0.0")
+						.description("API documentation for the Phantom Mask platform.")
+				);
+	}
+}
